42e7c5b71603fbbb63fb25c124d47b56776220fc
Michael J. Kozubal Jr <mjkjr84@gmail.com>
2022-05-03 20:29:21 -0400

    Completed project to pass fcc tests

--- server.js
+++ server.js
@@ -1,20 +1,30 @@
-var express = require('express');
-var cors = require('cors');
 require('dotenv').config()
 
+var express = require('express');
 var app = express();
 
+var multer = require( 'multer' );
+const upload = multer( { dest: 'uploads/' } );
+
+var cors = require('cors');
 app.use(cors());
+
 app.use('/public', express.static(process.cwd() + '/public'));
 
 app.get('/', function (req, res) {
-    res.sendFile(process.cwd() + '/views/index.html');
+	res.sendFile(process.cwd() + '/views/index.html');
 });
 
+app.post( '/api/fileanalyse', upload.single('upfile'), ( request, response) => {
 
-
+	response.json( {
+		name:request.file.originalname,
+		type:request.file.mimetype,
+		size:request.file.size
+	} );
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
+	console.log('Your app is listening on port ' + port)
 });
